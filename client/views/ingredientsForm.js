d6eb1fe005b00c46c315d9cf1882ddc959a6f666
anna-marie-vos <amvos@outlook.com>
2017-01-12 17:15:29 +1300

    final commit of the day

--- client/views/ingredientsForm.js
+++ client/views/ingredientsForm.js
@@ -2,18 +2,15 @@ module.exports = function(){
   return (
     `<form action="#">
     <h2>Ingredients</h2>
-    <input type="checkbox" name="ingredientID" value = "cheese"> Cheese<br>
-    <input type="checkbox" name="ingredientID" value = "feta"> feta<br>
-    <input type="checkbox" name="ingredientID" value = "orange"> orange<br>
-    <input type="checkbox" name="ingredientID" value = "mint"> mint<br>
-    <input type="checkbox" name="ingredientID" value = "fig jam"> fig jam<br>
-    <input type="checkbox" name="ingredientID" value = "ham"> ham<br>
-    <input type="checkbox" name="ingredientID" value = "walnuts"> walnuts<br>
-    <input type="checkbox" name="ingredientID" value = "butter"> butter<br>
-    <input type="button" name="submit" value = "submit">
-
-
-
+      <input type="checkbox" name="ingredientID" value = "cheese"> Cheese<br>
+      <input type="checkbox" name="ingredientID" value = "feta"> feta<br>
+      <input type="checkbox" name="ingredientID" value = "orange"> orange<br>
+      <input type="checkbox" name="ingredientID" value = "mint"> mint<br>
+      <input type="checkbox" name="ingredientID" value = "fig jam"> fig jam<br>
+      <input type="checkbox" name="ingredientID" value = "ham"> ham<br>
+      <input type="checkbox" name="ingredientID" value = "walnuts"> walnuts<br>
+      <input type="checkbox" name="ingredientID" value = "butter"> butter<br>
+      <input type="button" name="submit" value = "submit">
     </form>`
   )
 }
