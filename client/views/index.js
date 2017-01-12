72928f85033b256f3adfbbf727009bd26e50347f
alice-youngquest <alice_youngquest@hotmail.com>
2017-01-13 08:35:22 +1300

    list of recipes template created

--- client/views/index.js
+++ client/views/index.js
@@ -1,16 +1,19 @@
 const _ = require('lodash')
 const form = require('./form')
 const ingredientsForm = require('./ingredientsForm')
+const listOfRecipes = require ('./listOfRecipes')
 const displaySpecificRecipe = require('./displaySpecificRecipe')
 
 module.exports = function (model) {
 
   return (
     `<div id="app">
-    <h1>My app: ${model.appName}</h1>
+    <h1>${model.appName}</h1>
       ${form()}
       ${ingredientsForm()}
+      ${listOfRecipes()}
       ${displaySpecificRecipe()}
+
 
     </div>`
   )
---
+++ client/views/listOfRecipes.js
@@ -0,0 +1,14 @@
+module.exports = function(){
+  return (
+
+   `
+    <h2>Recipes you matched with</h2>
+      <ul>
+        <li>Fancy toast</li>
+        <li>Crazy toast</li>
+        <li>Basic toast</li>
+      </ul>
+
+   `
+  )
+}
