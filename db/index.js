a0ce6cf24b74cefe46444e0ffc957c004dfe9ae0
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-01-13 12:28:52 +1300

    second test works

--- db/index.js
+++ db/index.js
@@ -15,16 +15,19 @@ module.exports = function (knex) {
       .join('ing_rec_join', 'recipes.id', '=', 'ing_rec_join.recipe_id')
       .join('ingredients', 'ingredients.id', '=', 'ing_rec_join.ingredient_id')
       .where('ingredient_id',ingredients[0].id)
-      .then(function(data){
+      .then(function(multiRecipes){
+
         const returnedData =
-        {
-          id: data[0].recipe_id,
-          recipe_name: data[0].recipe_name,
-          method: data[0].method,
-          image: data[0].image
-        }
+        multiRecipes.map(function(oneRecipe){
+          return {
+            id: oneRecipe.recipe_id,
+            recipe_name: oneRecipe.recipe_name,
+            method: oneRecipe.method,
+            image: oneRecipe.image
+          }
+        })
         console.log(returnedData);
-        return [returnedData]
+        return returnedData
       })
       // .where('')
       // .then(function(data){
--- test/db/index-test.js
+++ test/db/index-test.js
@@ -20,7 +20,7 @@ test.afterEach.always(() => {
   return testKnex.migrate.rollback()
 })
 
-test.only('findRecipesByIngredients | returns all recipes from db which include no more than the submitted ingredients', (t) => {
+test('findRecipesByIngredients | returns the bacon recipes ', (t) => {
   t.plan(1)
 
   //Arrange
@@ -41,5 +41,39 @@ test.only('findRecipesByIngredients | returns all recipes from db which include 
     //Assert
     t.deepEqual(data,expected)
   })
+})
 
+test('findRecipesByIngredients | returns the egg recipes ', (t) => {
+  t.plan(1)
+
+  //Arrange
+  const ingredients = [{id: 1}] //OR {cheese: true, eggs: true, bacon: false}
+  const expected = [
+    {
+      id: 1,
+      recipe_name: 'poached egg',
+      method: 'Add a little vinegar to water just coming to the boil. Turn off the heat and gently break eggs into the water. Leave for 3 mins. Drain eggs well before putting onto toast',
+      image: 'http://www.jamieshomecookingskills.com/core_jo/images/jhcs/main-image-2556.jpg'
+    },
+    {
+      id: 2,
+      recipe_name: 'omelette',
+      method: 'Break two eggs into a bowl and whisk well with a little grated cheese. Melt butter in a pan and cook on a low heat, constantly stirring until just cooked.',
+      image: 'http://www.bite.co.nz/images/recipes/exam_eggs_bite.jpg'
+    },
+    {
+      id: 3,
+      recipe_name: 'bacon and eggs',
+      method: 'Cook bacon. Poach eggs. Add toast',
+      image: 'http://www.eggcrackin.com/wp-content/uploads/2015/12/047a9eff.product_classic.png'
+    }
+  ]
+
+  //Act
+  return db.findRecipesByIngredients(ingredients)
+  .then(function(data){
+
+    //Assert
+    t.deepEqual(data,expected)
+  })
 })
